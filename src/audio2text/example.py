c8adafe03826964cb9f41e76349679d88b028c7f
Gleb Neshchetkin <glebneshchetkin@gmail.com>
2025-03-02 17:25:13 +0300

    CI fix2

--- src/audio2text/example.py
+++ src/audio2text/example.py
@@ -2,7 +2,7 @@ from huggingsound import SpeechRecognitionModel
 from utils import *
 
 model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
-real_transcriptions = load_data_golos('./audio_files')
+real_transcriptions = load_data_golos("./audio_files")
 
 # Generate transcriptions for golos data
 generated_transcriptions = generate_transcriptions_golos(model)
@@ -10,6 +10,5 @@ generated_transcriptions = generate_transcriptions_golos(model)
 # Calculate test metrics
 calc_test_metrics(real_transcriptions, generated_transcriptions)
 
-generated_transcription = generate_transcription(model, './audio_files/output_321.wav')
+generated_transcription = generate_transcription(model, "./audio_files/output_321.wav")
 print(generated_transcription)
-
