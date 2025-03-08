55c9d74daaf24b039986d56fe6e1eb2833758b78
Gleb Neshchetkin <glebneshchetkin@gmail.com>
2025-03-08 12:44:28 +0300

    whisper model added(4)

--- src/audio2text/utils.py
+++ src/audio2text/utils.py
@@ -76,7 +76,7 @@ def gen_transcription_whisper(
         input_features = processor(audio_input, return_tensors="pt").input_features
         with torch.no_grad():
             generated_ids = model.generate(input_features)
-            transcription = processor.decode(generated_ids[0], skip_special_tokens=True)
+        transcription = processor.decode(generated_ids[0], skip_special_tokens=True)
         transcription = cast(str, transcription) if transcription else ""
         return transcription
     raise FileNotFoundError(f"No file {path_to_file}")
