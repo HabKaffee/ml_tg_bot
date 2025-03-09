c63d33bc019c90fa20e4867c63e72c4e70081850
Trutnev Aleksei <alexgiving@mail.ru>
2025-03-09 16:32:08 +0300

    Clean Up (#8)
    This commit includes several changes to the config/py_init_checker.py file to add functionality for excluding certain directories, updates to the config/run_lint.sh script, and the removal of the TelegramMockAPI class from src/telegram_api/mocked_api.py.
    
    Enhancements to config/py_init_checker.py:
    
    - Added exclude parameter to ArgumentParser class and updated check_init_files function to handle excluded directories.
    - Modified the script entry point to pass the exclude parameter to the check_init_files function.
    Updates to linting script:
    
    - Simplified the config/run_lint.sh script by consolidating the py_init_checker.py call and reordering the pylint commands.
    Removal of unused code:
    
    - Removed the TelegramMockAPI class from src/telegram_api/mocked_api.py as it was no longer needed.

--- config/run_lint.sh
+++ config/run_lint.sh
@@ -2,8 +2,7 @@
 
 source config/bootstrap.sh
 
-python config/py_init_checker.py --directory config/
-pylint config/ --rcfile config/.pylintrc
+python config/py_init_checker.py --directory .
 
-python config/py_init_checker.py --directory src/
+pylint config/ --rcfile config/.pylintrc
 pylint src/ --rcfile config/.pylintrc
