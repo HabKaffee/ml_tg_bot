811bc9341978b2add01e735c6a918c0afaf085cf
o-frolova <olga.frolova.01@yandex.ru>
2025-03-09 12:41:12 +0300

    fix ci

--- src/image_generation/generation.py
+++ src/image_generation/generation.py
@@ -1,11 +1,12 @@
-import PIL
 from diffusers import DiffusionPipeline
+from PIL import Image
+
 
 class StableDiffusionPipeline:
-    def __init__(self, model_name: str = "stable-diffusion-v1-5/stable-diffusion-v1-5"):
-        self._model_name = model_name
-        self._pipeline = DiffusionPipeline.from_pretrained(self._model_name)
-    
-    def generate_image(self, prompt: str) -> PIL.Image.Image:
-        image = self._pipeline(prompt).images[0]
+    def __init__(self, model_path: str = "stable-diffusion-v1-5/stable-diffusion-v1-5"):
+        self._model_path = model_path
+        self._pipeline = DiffusionPipeline.from_pretrained(self._model_path)
+
+    def generate_image(self, prompt: str) -> Image.Image:
+        image: Image.Image = self._pipeline(prompt).images[0]
         return image
