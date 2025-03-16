e2c85cae553eb9d122fdbd481787486dbfdf4e28
Vladislav-Kanev <71171071+Vladislav-Kanev@users.noreply.github.com>
2025-03-16 10:06:47 +0300

    Add sticker generator (#14)
    This pull request includes several changes to improve the functionality and organization of the image processing and sticker generation modules. The key changes involve fixing a typo in a file path, refactoring utility functions, and adding a new sticker generator feature.
    
    - Added a new StickerGenerator class to generate stickers by segmenting the main object in an image using a model.
    - Added a new example script to test the StickerGenerator functionality

--- src/image_processing/example.py
+++ src/image_processing/example.py
@@ -1,24 +1,8 @@
-from io import BytesIO
-
-import requests
-from PIL import Image
 from tap import Tap
 
 from src.image_processing.command_parser.command_parser_creator import CommandParserTypes, get_command_parser
 from src.image_processing.image_processor import ImageProcessor
-
-
-def get_random_cat_image() -> Image.Image:
-    url = "https://api.thecatapi.com/v1/images/search"
-    response = requests.get(url, timeout=25)
-    response.raise_for_status()
-    image_url = response.json()[0]["url"]
-
-    image_response = requests.get(image_url, timeout=25)
-    image_response.raise_for_status()
-
-    image = Image.open(BytesIO(image_response.content))
-    return image
+from src.utils import get_random_cat_image
 
 
 class Arguments(Tap):
